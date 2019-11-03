ecfe484c4898a7d8e6fd093225b90f06fc80e7e8
williamchand <williamchans57@gmail.com>
2019-11-03 07:04:15 +0700

    fuckwr bpwajeowkq'

--- src/components/component/home/datemusic.js
+++ src/components/component/home/datemusic.js
@@ -8,6 +8,7 @@ import ReactAudioPlayer from 'react-audio-player';
 function DateMusicView() {
   const [userlat, setUserLat] = useState(43.855472);
   const [userlng, setUserLng] = useState(18.410574);
+  const [weather, setWeather] = useState({});
   
   React.useEffect(() => {
     navigator.geolocation.watchPosition(function(position) {
@@ -17,12 +18,23 @@ function DateMusicView() {
   },
     [setUserLng, setUserLat]
   );
-
+  React.useEffect(()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${userlat}&lon=${userlng}&appid=379c73d39ee69ae7ab75561b09a35cdf`).then(res => res.json())
+      .then(response => setWeather(response));
+  }, [userlat, userlng]);
+  console.log(weather);
   return (
     <Card style={{backgroundColor: 'transparent'}}>
       <Card.Body>
         <div style={{display: 'flex'}}>
-          <Clock />
+        <p><Clock /></p>
+        <p style={{color:'white'}}>Jakarta Now { weather.main && weather.main.temp ? (weather.main.temp-273.15).toFixed(2) : ''} Celcius with { weather.weather && weather.weather[0].description ? weather.weather[0].description : ''}</p>
+          <div style={{color:'white'}}>
+            <Card style={{border: '0px', backgroundColor: 'transparent'}}>
+              <Card.Body style={{padding: '0px'}}>
+              </Card.Body>
+            </Card>
+          </div>
           <ReactAudioPlayer
             src="http://streaming.tdiradio.com:8000/house.mp3"
             autoPlay
